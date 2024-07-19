2c718bd185e864267703829e4317f11f88314e0b
SergioLM <sergiolillom@gmail.com>
2024-07-19 14:42:24 +0200

    myStats v1 OK: get y renderizado

--- client/src/components/Main/MyStats/MyStats.jsx
+++ client/src/components/Main/MyStats/MyStats.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from 'axios';
 import { MensajeError } from '../../../context/MensajeError';
+import Header from '../../Header/Header';
+import NavUser from '../../NavUser/NavUser';
 import { DNA } from 'react-loader-spinner';
 import Cookies from 'js-cookie';
 import {jwtDecode} from 'jwt-decode';
 
-const MyStats = () => {
+const MyStats = ({ handleLogout }) => {
   const { error, setError } = useContext(MensajeError);
   const [myStats, setMyStats] = useState([]);
   const [searching, setSearching] = useState(false);
@@ -42,7 +44,7 @@ const MyStats = () => {
     try {
       const res = await axios.get(`https://data-geri.onrender.com/api/stats/${email}`);
       console.log(res);
-      //setMyStats(res.data);
+      setMyStats([res.data[0]]);
       setError('');
     } catch (err) {
       console.error('Error al traer las estadísticas de la base de datos', err);
@@ -57,11 +59,13 @@ const MyStats = () => {
   };
 
   const formatNumber = (num) => {
-    return parseFloat(num).toFixed(2);
+    return Number(parseFloat(num).toFixed(3));
   };
 
-  return (
-    <section>
+  return <>
+    <Header />
+    <NavUser handleLogout={handleLogout} />
+    <section className="my-stats">
       <h2>My Stats</h2>
       {searching ? (
         <DNA
@@ -76,16 +80,16 @@ const MyStats = () => {
         <article className="stats-user">
           {myStats.length > 0 ? (
              <ul>
-             <li>Edad media: {formatNumber(myStats.edadmedia)} años</li>
-             <li>Estancia media: {formatNumber(myStats.estanciamedia)} días</li>
-             <li>Total ingresos: {myStats.totalingresos}</li>
-             <li>Porcentaje de hombres: {formatNumber(myStats.porcentajehombres)}%</li>
-             <li>Porcentaje de mujeres: {formatNumber(myStats.porcentajemujeres)}%</li>
-             <li>Porcentaje ITU: {formatNumber(myStats.porcentajeitu)}%</li>
-             <li>Porcentaje neumonía: {formatNumber(myStats.porcentajeneumonia)}%</li>
-             <li>Porcentaje ICC: {formatNumber(myStats.porcentajeicc)}%</li>
-             <li>Porcentaje infección abdominal: {formatNumber(myStats.porcentajeinfeccabd)}%</li>
-             <li>Porcentaje otros: {formatNumber(myStats.porcentajeotro)}%</li>
+             <li>Edad media: {formatNumber(myStats[0].edadmedia)} años</li>
+             <li>Estancia media: {formatNumber(myStats[0].estanciamedia)} días</li>
+             <li>Total ingresos: {myStats[0].totalingresos}</li>
+             <li>Porcentaje de hombres: {formatNumber(myStats[0].porcentajehombres)}%</li>
+             <li>Porcentaje de mujeres: {formatNumber(myStats[0].porcentajemujeres)}%</li>
+             <li>Porcentaje ITU: {formatNumber(myStats[0].porcentajeitu)}%</li>
+             <li>Porcentaje neumonía: {formatNumber(myStats[0].porcentajeneumonia)}%</li>
+             <li>Porcentaje ICC: {formatNumber(myStats[0].porcentajeicc)}%</li>
+             <li>Porcentaje infección abdominal: {formatNumber(myStats[0].porcentajeinfeccabd)}%</li>
+             <li>Porcentaje otros: {formatNumber(myStats[0].porcentajeotro)}%</li>
            </ul>
           ) : (
             <p>No hay estadísticas disponibles.</p>
@@ -94,7 +98,7 @@ const MyStats = () => {
       )}
       {error && <p className="error-message">{error}</p>}
     </section>
-  );
+    </>;
 };
 
 export default MyStats;
