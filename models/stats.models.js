a13fd1b34dd4bbcb28854c06baa8931ae1822540
SergioLM <sergiolillom@gmail.com>
2024-07-21 21:42:35 +0200

    jsdoc OK

--- models/stats.models.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author Sergio Lillo <Data Geri> 
- * @exports models
- * @namespace Models.stats
- */
-const { getClient } = require('../config/db_sql');
-const queries = require('../queries/stats.queries');
-
